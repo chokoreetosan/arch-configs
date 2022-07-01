0447526855ac06fd8394d411a0a49953b5c4722c
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2022-07-01 06:41:36 -0700

    added config files. thiy should be defaults

---
+++ .config/aconfmgr/10-ignores.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+IgnorePath '/etc/*'
+IgnorePath '/boot/*'
+IgnorePath '/etc/.pwd.lock'
+IgnorePath '/etc/.updated'
+IgnorePath '/etc/ca-certificates/*'
+IgnorePath '/etc/ld-so.cache'
+IgnorePath '/etc/machine-id'
+IgnorePath '/etc/pacman.d/gnupg/*'
+IgnorePath '/etc/ssl/certs/*'
+IgnorePath '/etc/lvm/*'
+IgnorePath '/etc/passwd-'
+IgnorePath '/etc/shadow-'
+IgnorePath '/etc/group-'
+IgnorePath '/etc/gshallow-'
+IgnorePath '/etc/pki/*'
+IgnorePath '/etc/light*'
+IgnorePath '/etc/ssh/ssh_host_*'
+IgnorePath '/etc/resolv.conf'
+IgnorePath '/etc/fonts/*'
+
+IgnorePath '/lost+found'
+
+IgnorePath '/usr/bin/*'
+IgnorePath '/usr/lib/*'
+IgnorePath '/usr/share/*'
+
+IgnorePath '/var/*'
+
+IgnorePath '*.bak'
+IgnorePath '*.sbak'
+IgnorePath '*.pacnew'
+IgnorePath '*.pacsave'
+
---
+++ .config/aconfmgr/99-unsorted.sh
@@ -0,0 +1,113 @@
+
+
+# Fri Jul  1 06:24:33 AM PDT 2022 - Unknown packages
+
+
+AddPackage alacritty # A cross-platform, GPU-accelerated terminal emulator
+AddPackage amd-ucode # Microcode update image for AMD CPUs
+AddPackage autoconf # A GNU tool for automatically configuring source code
+AddPackage automake # A GNU tool for automatically creating Makefiles
+AddPackage awesome # Highly configurable framework window manager
+AddPackage base # Minimal package set to define a basic Arch Linux installation
+AddPackage binutils # A set of programs to assemble and manipulate binary and object files
+AddPackage bison # The GNU general-purpose parser generator
+AddPackage cmake # A cross-platform open-source make system
+AddPackage dhcpcd # RFC2131 compliant DHCP client daemon
+AddPackage efibootmgr # Linux user-space application to modify the EFI Boot Manager
+AddPackage fakeroot # Tool for simulating superuser privileges
+AddPackage file # File type identification utility
+AddPackage findutils # GNU utilities to locate files
+AddPackage firefox # Standalone web browser from mozilla.org
+AddPackage flex # A tool for generating text-scanning programs
+AddPackage gawk # GNU version of awk
+AddPackage gcc # The GNU Compiler Collection - C and C++ frontends
+AddPackage gettext # GNU internationalization library
+AddPackage git # the fast distributed version control system
+AddPackage github-cli # The GitHub CLI
+AddPackage grep # A string search utility
+AddPackage groff # GNU troff text-formatting system
+AddPackage grub # GNU GRand Unified Bootloader (2)
+AddPackage gzip # GNU compression utility
+AddPackage hwinfo # Hardware detection tool from openSUSE
+AddPackage libtool # A generic library support script
+AddPackage linux # The Linux kernel and modules
+AddPackage linux-firmware # Firmware files for Linux
+AddPackage lsof # Lists open files for running Unix processes
+AddPackage m4 # The GNU macro processor
+AddPackage make # GNU make utility to maintain groups of programs
+AddPackage nano # Pico editor clone with enhancements
+AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
+AddPackage netctl # Profile based systemd network management
+AddPackage nitrogen # Background browser and setter for X windows
+AddPackage npm # A package manager for javascript
+AddPackage pacman # A library-based package manager with dependency support
+AddPackage patch # A utility to apply patch files to original sources
+AddPackage picom # X compositor that may fix tearing issues
+AddPackage pkgconf # Package compiler and linker metadata toolkit
+AddPackage rust # Systems programming language focused on safety, speed and concurrency
+AddPackage sed # GNU stream editor
+AddPackage sudo # Give certain users the ability to run some commands as root
+AddPackage texinfo # GNU documentation system for on-line information and printed output
+AddPackage tmux # A terminal multiplexer
+AddPackage vi # The original ex/vi text editor
+AddPackage virtualbox-guest-utils # VirtualBox Guest userspace utilities
+AddPackage which # A utility to show the full path of commands
+AddPackage xf86-video-vesa # X.org vesa video driver
+AddPackage xorg-bdftopcf # Convert X font from Bitmap Distribution Format to Portable Compiled Format
+AddPackage xorg-docs # X.org documentations
+AddPackage xorg-font-util # X.Org font utilities
+AddPackage xorg-fonts-100dpi # X.org 100dpi fonts
+AddPackage xorg-fonts-75dpi # X.org 75dpi fonts
+AddPackage xorg-iceauth # ICE authority file utility
+AddPackage xorg-mkfontscale # Create an index of scalable font files for X
+AddPackage xorg-server # Xorg X server
+AddPackage xorg-server-devel # Development files for the X.Org X server
+AddPackage xorg-server-xephyr # A nested X server that runs as an X application
+AddPackage xorg-server-xnest # A nested X server that runs as an X application
+AddPackage xorg-server-xvfb # Virtual framebuffer X server
+AddPackage xorg-sessreg # Register X sessions in system utmp/utmpx databases
+AddPackage xorg-smproxy # Allows X applications that do not support X11R6 session management to participate in an X11R6 session
+AddPackage xorg-twm # Tab Window Manager for the X Window System
+AddPackage xorg-x11perf # Simple X server performance benchmarker
+AddPackage xorg-xbacklight # RandR-based backlight control application
+AddPackage xorg-xclock # X clock
+AddPackage xorg-xcmsdb # Device Color Characterization utility for X Color Management System
+AddPackage xorg-xcursorgen # Create an X cursor file from PNG images
+AddPackage xorg-xdpyinfo # Display information utility for X
+AddPackage xorg-xdriinfo # Query configuration information of DRI drivers
+AddPackage xorg-xev # Print contents of X events
+AddPackage xorg-xgamma # Alter a monitor's gamma correction
+AddPackage xorg-xhost # Server access control program for X
+AddPackage xorg-xinit # X.Org initialisation program
+AddPackage xorg-xinput # Small commandline tool to configure devices
+AddPackage xorg-xkbevd # XKB event daemon
+AddPackage xorg-xkbutils # XKB utility demos
+AddPackage xorg-xkill # Kill a client by its X resource
+AddPackage xorg-xlsatoms # List interned atoms defined on server
+AddPackage xorg-xlsclients # List client applications running on a display
+AddPackage xorg-xlsfonts # List available X fonts
+AddPackage xorg-xpr # Print an X window dump from xwd
+AddPackage xorg-xprop # Property displayer for X
+AddPackage xorg-xrefresh # Refresh all or part of an X screen
+AddPackage xorg-xset # User preference utility for X
+AddPackage xorg-xsetroot # Classic X utility to set your root window background to a given pattern or color
+AddPackage xorg-xvinfo # Prints out the capabilities of any video adaptors associated with the display that are accessible through the X-Video extension
+AddPackage xorg-xwayland # run X clients under wayland
+AddPackage xorg-xwd # X Window System image dumping utility
+AddPackage xorg-xwininfo # Command-line utility to print information about windows on an X server
+AddPackage xorg-xwud # X Window System image undumping utility
+AddPackage xterm # X Terminal Emulator
+
+
+# Fri Jul  1 06:24:34 AM PDT 2022 - Unknown foreign packages
+
+
+AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
+AddPackage --foreign tmux-bash-completion-git # Bash completion for tmux
+AddPackage --foreign yay-git # Yet another yogurt. Pacman wrapper and AUR helper written in go. (development version)
+
+
+# Fri Jul  1 06:24:34 AM PDT 2022 - New / changed files
+
+
+CopyFile /lvim 744
