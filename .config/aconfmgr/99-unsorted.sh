4f695c51f294e5340eb5714b0e7b4459aa9a02c9
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2022-07-03 14:09:11 -0700

    moved some dependencies to their own files in aconfmgr and now we are specifiying xorg group instead of each of the packages in aconfmgr.

--- .config/aconfmgr/99-unsorted.sh
+++ .config/aconfmgr/99-unsorted.sh
@@ -1,13 +1,11 @@
 
 
-# Sat Jul  2 11:20:36 PM PDT 2022 - Unknown packages
+# Sun Jul  3 02:07:05 PM PDT 2022 - Unknown packages
 
 
-AddPackage alacritty # A cross-platform, GPU-accelerated terminal emulator
 AddPackage amd-ucode # Microcode update image for AMD CPUs
 AddPackage autoconf # A GNU tool for automatically configuring source code
 AddPackage automake # A GNU tool for automatically creating Makefiles
-AddPackage awesome # Highly configurable framework window manager
 AddPackage base # Minimal package set to define a basic Arch Linux installation
 AddPackage binutils # A set of programs to assemble and manipulate binary and object files
 AddPackage bison # The GNU general-purpose parser generator
@@ -42,7 +40,6 @@ AddPackage netctl # Profile based systemd network management
 AddPackage npm # A package manager for javascript
 AddPackage pacman # A library-based package manager with dependency support
 AddPackage patch # A utility to apply patch files to original sources
-AddPackage picom # X compositor that may fix tearing issues
 AddPackage pkgconf # Package compiler and linker metadata toolkit
 AddPackage rust # Systems programming language focused on safety, speed and concurrency
 AddPackage sed # GNU stream editor
@@ -53,53 +50,34 @@ AddPackage vi # The original ex/vi text editor
 AddPackage virtualbox-guest-utils # VirtualBox Guest userspace utilities
 AddPackage which # A utility to show the full path of commands
 AddPackage xf86-video-vesa # X.org vesa video driver
-AddPackage xorg-bdftopcf # Convert X font from Bitmap Distribution Format to Portable Compiled Format
 AddPackage xorg-docs # X.org documentations
 AddPackage xorg-font-util # X.Org font utilities
 AddPackage xorg-fonts-100dpi # X.org 100dpi fonts
 AddPackage xorg-fonts-75dpi # X.org 75dpi fonts
-AddPackage xorg-iceauth # ICE authority file utility
-AddPackage xorg-mkfontscale # Create an index of scalable font files for X
-AddPackage xorg-server # Xorg X server
 AddPackage xorg-server-devel # Development files for the X.Org X server
 AddPackage xorg-server-xephyr # A nested X server that runs as an X application
 AddPackage xorg-server-xnest # A nested X server that runs as an X application
 AddPackage xorg-server-xvfb # Virtual framebuffer X server
-AddPackage xorg-sessreg # Register X sessions in system utmp/utmpx databases
-AddPackage xorg-smproxy # Allows X applications that do not support X11R6 session management to participate in an X11R6 session
 AddPackage xorg-twm # Tab Window Manager for the X Window System
-AddPackage xorg-x11perf # Simple X server performance benchmarker
-AddPackage xorg-xbacklight # RandR-based backlight control application
 AddPackage xorg-xclock # X clock
-AddPackage xorg-xcmsdb # Device Color Characterization utility for X Color Management System
-AddPackage xorg-xcursorgen # Create an X cursor file from PNG images
-AddPackage xorg-xdpyinfo # Display information utility for X
-AddPackage xorg-xdriinfo # Query configuration information of DRI drivers
-AddPackage xorg-xev # Print contents of X events
-AddPackage xorg-xgamma # Alter a monitor's gamma correction
-AddPackage xorg-xhost # Server access control program for X
 AddPackage xorg-xinit # X.Org initialisation program
-AddPackage xorg-xinput # Small commandline tool to configure devices
-AddPackage xorg-xkbevd # XKB event daemon
-AddPackage xorg-xkbutils # XKB utility demos
-AddPackage xorg-xkill # Kill a client by its X resource
-AddPackage xorg-xlsatoms # List interned atoms defined on server
-AddPackage xorg-xlsclients # List client applications running on a display
 AddPackage xorg-xlsfonts # List available X fonts
-AddPackage xorg-xpr # Print an X window dump from xwd
-AddPackage xorg-xprop # Property displayer for X
-AddPackage xorg-xrefresh # Refresh all or part of an X screen
-AddPackage xorg-xset # User preference utility for X
-AddPackage xorg-xsetroot # Classic X utility to set your root window background to a given pattern or color
-AddPackage xorg-xvinfo # Prints out the capabilities of any video adaptors associated with the display that are accessible through the X-Video extension
 AddPackage xorg-xwayland # run X clients under wayland
-AddPackage xorg-xwd # X Window System image dumping utility
-AddPackage xorg-xwininfo # Command-line utility to print information about windows on an X server
-AddPackage xorg-xwud # X Window System image undumping utility
 AddPackage xterm # X Terminal Emulator
 
 
-# Sat Jul  2 11:20:37 PM PDT 2022 - Unknown foreign packages
+# Sun Jul  3 02:07:06 PM PDT 2022 - Missing packages
+
+
+RemovePackage xorg-setxkbmap
+RemovePackage xorg-xauth
+RemovePackage xorg-xkbcomp
+RemovePackage xorg-xmodmap
+RemovePackage xorg-xrandr
+RemovePackage xorg-xrdb
+
+
+# Sun Jul  3 02:07:06 PM PDT 2022 - Unknown foreign packages
 
 
 AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
@@ -108,7 +86,7 @@ AddPackage --foreign tmux-bash-completion-git # Bash completion for tmux
 AddPackage --foreign yay-git # Yet another yogurt. Pacman wrapper and AUR helper written in go. (development version)
 
 
-# Sat Jul  2 11:20:38 PM PDT 2022 - New / changed files
+# Sun Jul  3 02:07:06 PM PDT 2022 - New / changed files
 
 
 CopyFile /lvim 744
