8220f8c5588d78e957efe290997cc3ee783fa1a9
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2022-07-06 21:00:18 -0700

    Added base-devel back in because yay needs it. This took care of quite a few packages in 99.

--- .config/aconfmgr/15-base.sh
+++ .config/aconfmgr/15-base.sh
@@ -2,6 +2,7 @@
 # These packages should be installed before running aconfmgr anyway. But, we're putting them here.
 
 AddPackage 'base' # Minimal package set to define a basic Arch Linux installation.
+AddPackageGroup 'base-devel'
 AddPackage 'linux' # The Linux kernel and modules.
 AddPackage 'linux-firmware' # Firmware files for linux.
 AddPackage 'git' # Version control tool.
--- .config/aconfmgr/99-unsorted.sh
+++ .config/aconfmgr/99-unsorted.sh
@@ -1,54 +1,21 @@
 
 
-# Wed Jul  6 05:15:17 PM PDT 2022 - Unknown packages
+# Wed Jul  6 08:59:41 PM PDT 2022 - Unknown packages
 
 
 AddPackage amd-ucode # Microcode update image for AMD CPUs
-AddPackage autoconf # A GNU tool for automatically configuring source code
-AddPackage automake # A GNU tool for automatically creating Makefiles
-AddPackage binutils # A set of programs to assemble and manipulate binary and object files
-AddPackage bison # The GNU general-purpose parser generator
 AddPackage cmake # A cross-platform open-source make system
 AddPackage dhcpcd # RFC2131 compliant DHCP client daemon
 AddPackage efibootmgr # Linux user-space application to modify the EFI Boot Manager
-AddPackage fakeroot # Tool for simulating superuser privileges
 AddPackage feh # Fast and light imlib2-based image viewer
-AddPackage file # File type identification utility
-AddPackage findutils # GNU utilities to locate files
-AddPackage flex # A tool for generating text-scanning programs
-AddPackage gawk # GNU version of awk
-AddPackage gcc # The GNU Compiler Collection - C and C++ frontends
-AddPackage gettext # GNU internationalization library
-AddPackage groff # GNU troff text-formatting system
 AddPackage grub # GNU GRand Unified Bootloader (2)
-AddPackage gzip # GNU compression utility
-AddPackage hwinfo # Hardware detection tool from openSUSE
-AddPackage libtool # A generic library support script
-AddPackage lsof # Lists open files for running Unix processes
-AddPackage m4 # The GNU macro processor
-AddPackage make # GNU make utility to maintain groups of programs
-AddPackage nano # Pico editor clone with enhancements
-AddPackage netctl # Profile based systemd network management
-AddPackage pacman # A library-based package manager with dependency support
-AddPackage patch # A utility to apply patch files to original sources
-AddPackage pkgconf # Package compiler and linker metadata toolkit
 AddPackage rust # Systems programming language focused on safety, speed and concurrency
-AddPackage sed # GNU stream editor
-AddPackage sudo # Give certain users the ability to run some commands as root
-AddPackage texinfo # GNU documentation system for on-line information and printed output
 AddPackage vi # The original ex/vi text editor
-AddPackage which # A utility to show the full path of commands
 AddPackage xf86-video-vmware # X.org vmware video driver
 AddPackage xterm # X Terminal Emulator
 
 
-# Wed Jul  6 05:15:18 PM PDT 2022 - Unknown foreign packages
-
-
-AddPackage --foreign tmux-bash-completion-git # Bash completion for tmux
-
-
-# Wed Jul  6 05:15:18 PM PDT 2022 - New / changed files
+# Wed Jul  6 08:59:42 PM PDT 2022 - New / changed files
 
 
 CopyFile /lvim 744
