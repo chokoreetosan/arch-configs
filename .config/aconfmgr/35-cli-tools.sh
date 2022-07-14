872d824b930e85f8a1dbb43818cd478a64255e06
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2022-07-13 21:34:28 -0700

    Added a file for cli tools in aconfmgr

---
+++ .config/aconfmgr/35-cli-tools.sh
@@ -0,0 +1,26 @@
+
+
+
+
+AddPackage lsof # Lists open files for running Unix processes
+AddPackage which # A utility to show the full path of commands
+
+
+AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
+AddPackage findutils # GNU utilities to locate files
+AddPackage fakeroot # Tool for simulating superuser privileges
+
+AddPackage file # File type identification utility
+AddPackage hwinfo # Hardware detection tool from openSUSE
+AddPackage grep # A string search utility
+AddPackage github-cli # The GitHub CLI
+
+AddPackage neofetch # A CLI system information tool written in BASH that supports displaying images.
+
+AddPackage lastpass-cli # LastPass command line interface tool
+AddPackage nano # Pico editor clone with enhancements
+
+AddPackage gzip # GNU compression utility
+AddPackage ranger # Simple, vim-like file manager
+
+AddPackage lsd # Modern ls with a lot of pretty colors and awesome icons
