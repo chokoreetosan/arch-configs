43d9b885647af0b148057157dac73b364f2436cf
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2022-07-14 17:16:04 -0700

    added some browsers

--- .config/aconfmgr/60-apps.sh
+++ .config/aconfmgr/60-apps.sh
@@ -7,5 +7,10 @@ AddPackage feh # Fast and light imlib2-based image viewer
 AddPackage --foreign visual-studio-code-bin # Visual Studio Code (vscode): Editor for building and debugging modern web and cloud applications (official binary version)
 AddPackage --foreign zoom # Video Conferencing and Web Conferencing Service
 AddPackage --foreign google-chrome # The popular and trusted web browser by Google (Stable Channel)
+
+
+AddPackage --foreign brave-bin # Web browser that blocks ads and trackers by default (binary release)
+AddPackage --foreign surf # A simple web browser based on WebKit/GTK+.
+
 AddPackage firefox # Standalone web browser from mozilla.org
 AddPackage discord # All-in-one voice and text chat for gamers that's free and secure.
