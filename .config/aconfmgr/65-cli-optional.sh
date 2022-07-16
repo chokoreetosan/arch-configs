7b0531a6783ac7f5e98f16450e1c0a44b0f445bf
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2022-07-16 02:49:15 -0700

    added new files for optional programs

---
+++ .config/aconfmgr/65-cli-optional.sh
@@ -0,0 +1,37 @@
+
+AddPackage lolcat # Okay, no unicorns. But rainbows!!
+
+
+# Sat Jul 16 01:28:55 AM PDT 2022 - Unknown packages
+
+
+AddPackage asciiquarium # An aquarium/sea animation in ASCII art
+AddPackage cmatrix # A curses-based scrolling 'Matrix'-like screen
+AddPackage cowfortune # Configurable fortune cookie proclaiming cow (and a few other creatures)
+AddPackage cowsay # Configurable talking cow (and a few other creatures)
+AddPackage figlet # A program for making large letters out of ordinary text
+AddPackage fortune-mod # The Fortune Cookie Program from BSD games
+AddPackage jp2a # A small utility for converting JPG images to ASCII
+
+
+# Sat Jul 16 01:28:55 AM PDT 2022 - Unknown foreign packages
+
+
+AddPackage --foreign weatherspect # A virtual weather environment in ASCII
+AddPackage --foreign zeitkatze # Precise CLI stopwatch with cat ASCII art
+
+
+# Sat Jul 16 01:42:51 AM PDT 2022 - Unknown packages
+
+
+AddPackage catimg # Print images in a terminal with 256 colors support
+AddPackage nyancat # Nyancat rendered in your terminal.
+AddPackage sl # Steam Locomotive runs across your terminal when you type "sl" as you meant to type "ls"
+
+
+# Sat Jul 16 01:42:51 AM PDT 2022 - Unknown foreign packages
+
+
+AddPackage --foreign ascii-rain-git # Comfy rain for your console written in C with Ncurses.
+AddPackage --foreign cbonsai # A bonsai tree generator, written in C using ncurses
+AddPackage --foreign toilet # free replacement for the FIGlet utility.
---
+++ .config/aconfmgr/70-apps-optional.sh
@@ -0,0 +1,6 @@
+
+
+
+
+
+AddPackage discord # All-in-one voice and text chat for gamers that's free and secure.
