f07355e3432308c178962237467f644710628514
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2022-07-19 20:25:54 -0700

    wallpaper script now uses fehbg upon resize,

--- .config/awesome/deco/wallpaper.lua
+++ .config/awesome/deco/wallpaper.lua
@@ -5,16 +5,17 @@ local beautiful = require("beautiful")
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 function set_wallpaper(s)
+  os.execute('~/.fegbh')
   -- Wallpaper
-  if beautiful.wallpaper then
-    local wallpaper = beautiful.wallpaper
-    -- If wallpaper is a function, call it with the screen
-    if type(wallpaper) == "function" then
-      wallpaper = wallpaper(s)
-    end
-    gears.wallpaper.maximized(wallpaper, s, true)
-  end
+--  if beautiful.wallpaper then
+--    local wallpaper = beautiful.wallpaper
+--    -- If wallpaper is a function, call it with the screen
+--    if type(wallpaper) == "function" then
+--      wallpaper = wallpaper(s)
+--    end
+--    gears.wallpaper.maximized(wallpaper, s, true)
+--  end
 end
-
+--
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
 screen.connect_signal("property::geometry", set_wallpaper)
